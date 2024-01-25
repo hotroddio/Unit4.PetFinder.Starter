d3ac2c34aad6fc2e658cf2f68fb8fcd9b875c4b0
John Rodriguez <hotroddio@gmail.com>
2024-01-24 21:51:59 -0500

    This is my final solution

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import the pets array from data.js
+const { Console } = require('console');
 const pets = require('./data');
 
 // init express app
@@ -6,11 +7,14 @@ const express = require('express');
 const app = express();
 
 const PORT = 8080;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+res.sendFile(__dirname + '/public/index.html');
 });
 
 // hello world route
@@ -21,31 +25,38 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
-
+    const owner = req.query.owner;
+    console.log(`This is my response ${owner}`);
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const pet = pets.find(pet => pet.owner.toLowerCase() === owner);
+    // return (pet);
+    console.log(`*** ${pets[0].owner}`);
+    console.log(`--- ${pet => pet.owner === owner}`);
+    console.log(`This is my other response ${pet}`);
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const dogName = req.params.name;
+    // console.log(dogName, "\n");
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
+    const pet = pets.find((pet) => {
+        return pet.name.toLowerCase() === dogName
+    });
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 app.listen(PORT, () => {
